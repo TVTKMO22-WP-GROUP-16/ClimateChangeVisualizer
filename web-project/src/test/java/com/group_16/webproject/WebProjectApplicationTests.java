c1d6fe75276d698267e7f01e3c360239de80d9f3
IAmSnode <nuutti28@gmail.com>
2023-03-17 12:26:15 +0200

    spring ja react alustettu

---
+++ web-project/src/test/java/com/group_16/webproject/WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group_16.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
