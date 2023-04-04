163fbf852dd1579bce94fad2f7ba18fdfb3657ac
IAmSnode <nuutti28@gmail.com>
2023-04-04 19:52:25 +0300

    reactilla saa haettua databasesta tietoa

--- web-project/src/main/java/com/group_16/webproject/Entities/Co2Share.java
+++ web-project/src/main/java/com/group_16/webproject/Entities/Co2Share.java
@@ -1,6 +1,6 @@
 package com.group_16.webproject.Entities;
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
--- web-project/web-app/src/pages/Charts2.js
+++ web-project/web-app/src/pages/Charts2.js
@@ -2,38 +2,47 @@ import React from "react";
 import "./Charts2.css";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from "react-chartjs-2";
-import co2EmissionData from "./data/Co2_emissions_by_sector.json";
-
+import axios from "axios";
+import { useState } from "react";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 
+
+
 export default function Charts2() {
+ 
+ function GetCo2SharesData (){
+ 
+   axios.get("http://localhost:8090/co2shares")
+    .then(res => {
+      console.log(res);
+      var res = res.data;
+      
+    }).catch(err => {
+      console.log(err);
+    })
+    
+  } 
   
-  
-  
-  
-  const data = { 
-  
-    labels : ["Energy", "Industrial processes", "Waste", "Agriculture, Forestry & Land Use (AFOLU)"],
+
+  const data = {
+
+    labels: ['jotain', 'jotain','kakkaa','lissaakakkaa'],
     datasets: [
-    {
+      {
         label: "CO2 Emissions",
-        data: [ {Sector: "Energy", Shares: 73.2},
-                {Sector: "Industrial processes", Shares: 5.2},
-                {Sector: "Waste", Shares: 3.2},
-                {Sector: "Agriculture, Forestry & Land Use (AFOLU)", Shares: 18.4}
-              ],
+        data: [1,2,3,4],
         backgroundColor: ["rgba(255, 99, 132, 0.5)", "rgba(54, 162, 235, 0.5)", "rgba(255, 206, 86, 0.5)", "rgba(75, 192, 192, 0.5)"],
         borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)"],
         borderWidth: 1,
-       
-       
+
+
       },
     ],
   };
 
   const options = {
-      responsive: true,
+    responsive: true,
     title: {
       display: true,
       position: "top",
@@ -52,11 +61,12 @@ export default function Charts2() {
     }
   };
 
-  
+
   return (
     <div className="charts2" >
       <div className="donut">
-      <Doughnut options={options} data={data}/>
+        <Doughnut options={options} data={data} />
+        <button onClick={GetCo2SharesData}>Get data</button>
       </div>
     </div>
   );
