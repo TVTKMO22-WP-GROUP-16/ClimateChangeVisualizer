df8fb9840ddb7a46186e0a7bc8946bf0574ad3d9
Pete Kontiokari <117116608+kontiok@users.noreply.github.com>
2023-04-27 13:57:32 +0300

    Tehty pohja CustomVizualization.js

---
+++ web-project/web-app/src/components/CustomVizualization.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from 'react-router-dom';
+
+import V1 from "./V1";
+import V2 from "./V2";
+import V3 from "./V3";
+import V4 from "./V4";
+import V5 from "./V5";
+
+export default function CustomVizualization() {
+    
+}
