582fa2860ed6c841dc5ed81aa7c43d3a9772aedd
mikkot666 <t1tami02@students.oamk.fi>
2023-04-27 11:13:35 +0300

    pieniä päivityksiä

--- web-project/web-app/src/components/V4.js
+++ web-project/web-app/src/components/V4.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { Line } from "react-chartjs-2";
 import axios from "axios";
-import { useMemo } from "react";
 
 export default function V4() {
   const [data, setData] = useState([]);
